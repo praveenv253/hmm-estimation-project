2b3132ffbdacaa2c1fb2bbdced68243076fde1df
Praveen Venkatesh <praveenv253@gmail.com>
2015-05-10 18:17:59 -0400

    Add title, legend, etc. for results plot.

--- display_results.py
+++ display_results.py
@@ -9,12 +9,25 @@ if __name__ == '__main__':
     y = data[nz_indices]
 
     gamma = np.load('output/gamma.npy')
+    mu = np.load('output/mu.npy')
+
     x = np.argmax(gamma, axis=0)                  # State
 
+    plt.plot(x)
+    plt.show()
+
     colors = ['b', 'g', 'y', 'r']
+    plt.figure()
     for i in range(3, -1, -1):
         xi = np.where(x == i)[0]
         color = colors[x[xi[0]]]
-        plt.stem(nz_indices[xi], y[xi], linefmt=color+'-', markerfmt=' ')
-    plt.show()
+        plt.stem(nz_indices[xi], y[xi], linefmt=color+'-', markerfmt=' ',
+                 basefmt=' ')
 
+    plt.title('Data, coloured by HMM state\n'
+              '(state defined by Poisson parameter $\mu$)')
+    plt.xlabel('Time slot')
+    plt.ylabel('Number of packets per time slot')
+    legend_text = tuple(r'$\mu = %f$' % mu[i] for i in range(3, -1, -1))
+    plt.legend(legend_text)
+    plt.show()
